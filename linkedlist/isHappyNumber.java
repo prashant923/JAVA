441ad662f4466c2260dc0fa757c945e0e98898bd
Prashant singh <112719877+prashant923@users.noreply.github.com>
2024-03-01 13:44:51 +0000

    isHappy

---
+++ linkedlist/isHappyNumber.java
@@ -0,0 +1,21 @@
+public class isHappyNumber{
+    public boolean isHappy(int n){
+         int slow = n;
+         int fast = n;
+
+         do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+         }
+         while(slow!=fast);
+    }
+    public int findSquare(int number){
+        int ans = 0;
+        while(number>0){
+            int rem = number%10;
+            ans += rem*rem;
+            number /= 10;
+        }
+        return ans;
+    }
+}
